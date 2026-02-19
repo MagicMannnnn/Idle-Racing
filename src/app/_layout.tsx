840751e50f6f4513054f1de3b24dc7833e6c01ca
George-FG <george.terry@frasers.group>
2026-02-19 11:17:00 +0000

    fix: icons

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -77,7 +77,6 @@ export default function RootLayout() {
           ),
         }}
       />
-      <Stack.Screen name="not-found" />
     </Stack>
   )
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,10 @@
-import { router } from 'expo-router'
+import { useRouter } from 'expo-router'
+import { useEffect } from 'react'
 
 export default function NotFound() {
-  router.replace('/home')
+  const router = useRouter()
+  useEffect(() => {
+    router.replace('/')
+  }, [router])
   return null
 }
