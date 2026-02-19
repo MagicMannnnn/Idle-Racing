20f6db6b33f5ccb28fd7253f6e0a5622f87c65f2
George-FG <george.terry@frasers.group>
2026-02-19 11:51:39 +0000

    fix: icons

--- app.config.js
+++ app.config.js
@@ -13,6 +13,7 @@ export default ({ config }) => {
     web: {
       ...(config.web ?? {}),
       output: 'static',
+      bundler: 'metro',
     },
 
     // GitHub Pages support uses this experimental baseUrl
